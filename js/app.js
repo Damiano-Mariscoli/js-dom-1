e2b9b601b73065e86044c910e256140424bea4d5
Damiano-Mariscoli <damiano.mariscoli2@gmail.com>
2024-10-22 16:18:59 +0200

    FIX: commenti e rimossi i debug

--- js/app.js
+++ js/app.js
@@ -1,9 +1,14 @@
+//1)
+
+//definisco un toggle che lavora prima su lamp-off e poi su lamp-on in base al click del bottone
+//definire la condizione per cambiare il testo del bottone in base alla presenza o meno della classe "on-off"
+
+
 const lampOff =  document.getElementById('lamp-off');
 const button = document.getElementById('button');
 const lampOn = document.getElementById('lamp-on')
 
-
-
+button.innerText = 'Accendi'
 button.addEventListener('click',  function(){
     console.log('ho cliccato sul primo bottone');
 
@@ -11,27 +16,35 @@ button.addEventListener('click',  function(){
     lampOn.classList.toggle('on-off');
 
     if (lampOn.classList.contains('on-off')) {
+        
         button.innerText = 'Accendi'
     } else {
+
         button.innerText = 'Spegni'
     }
     
 })
 
+//2)
+
 //creare una funzione che pone la seguente condizione
 //se (la src di lampOff contiene al suo interno "white_lamp")
 //allora cambia la src di lampOff con quella di "yello_lamp"
+//altrimenti inserisci la src di "white_lamp"
+
+
 const lamp = document.getElementById('lamp')
 const secondButton = document.getElementById('second-button')
 
+secondButton.innerText = 'Accendi'
 secondButton.addEventListener('click' ,  function(){
     console.log('ho cliccato sul secondo bottone')
 
     if(lamp.src.includes('white_lamp')){
-        console.log('debug')
+        
         lamp.src = "./img/yellow_lamp.png"
     }else {
-        console.log('debug2')
+        
         lamp.src = "./img/white_lamp.png"
     }
 })
